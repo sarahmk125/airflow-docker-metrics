ea59bdfafcef144e416d06d43b33186c088590ce
Sarah Krasnik <sarah@Sarahs-MBP.fios-router.home>
2021-07-06 06:59:51 -0400

    creating lib dirs in good dags

--- dags/bad_dags/dag1.py
+++ dags/bad_dags/dag1.py
@@ -1,35 +1,35 @@
-import io
-import time
-from datetime import datetime, timedelta
-from time import sleep
+# import io
+# import time
+# from datetime import datetime, timedelta
+# from time import sleep
 
-import pandas as pd
-import requests
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+# import pandas as pd
+# import requests
+# from airflow import DAG
+# from airflow.operators.bash_operator import BashOperator
 
-# Default parameters
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2020, 9, 1),
-    "retries": 1,
-    "retry_delay": timedelta(minutes=2),
-}
-
-
-# At 6am every day
-schedule = "0 6 * * *"
+# # Default parameters
+# default_args = {
+#     "owner": "airflow",
+#     "depends_on_past": False,
+#     "start_date": datetime(2020, 9, 1),
+#     "retries": 1,
+#     "retry_delay": timedelta(minutes=2),
+# }
 
 
-# Declare dag
-dag = DAG("sample", default_args=default_args, schedule_interval=schedule)
+# # At 6am every day
+# schedule = "0 6 * * *"
 
 
-# Instantiate sample bash operator
-t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
-t2 = BashOperator(task_id="sleep", bash_command="sleep 1", dag=dag)
+# # Declare dag
+# dag = DAG("sample", default_args=default_args, schedule_interval=schedule)
 
 
-# Set dependencies
-t1 >> t2
+# # Instantiate sample bash operator
+# t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
+# t2 = BashOperator(task_id="sleep", bash_command="sleep 1", dag=dag)
+
+
+# # Set dependencies
+# t1 >> t2
---
+++ dags/good_dags/stock_retrieve/lib/stock_history.py
@@ -0,0 +1,38 @@
+import io
+from time import sleep
+
+import pandas as pd
+import requests
+from good_dags.stock_retrieve.lib.epoch_manipulate import EpochManipulate
+
+
+class StockHistory(object):
+    def __init__(self):
+        self.epoch = EpochManipulate()
+
+    def stock_retrieve(
+        self, stock, period1=None, period2=None, interval="1d", events="history", **kwargs
+    ):
+        period1 = self.epoch._get_previous_epoch_days(7) if not period1 else period1
+        period2 = self.epoch._get_current_epoch() if not period2 else period2
+
+        print(
+            f"[DataRetriever] Getting data for: stock {stock}, {period1} to {period2}, interval: {interval}, events: {events}..."
+        )
+
+        stock = stock.strip()
+        url = f"https://query1.finance.yahoo.com/v7/finance/download/{stock}?period1={period1}&period2={period2}&interval={interval}&events={events}"
+        response = requests.get(url)
+
+        if not response.ok:
+            # Retry one time due to page not found
+            print("[DataRetriever] Failure occurred, waiting 10 seconds then retrying once...")
+            sleep(10)
+
+            response = requests.get(url)
+            if not response.ok:
+                raise Exception(f"[DataRetriever] Yahoo request error. Response: {response.text}")
+
+        data = response.content.decode("utf8")
+        df_history = pd.read_csv(io.StringIO(data))
+        print(df_history)
